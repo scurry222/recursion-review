d3cee7e5c97affba52aef0829cf1926b0a690593
Jason Horn <jasonh.horn1@gmail.com>
2020-11-16 16:22:07 -0800

    add stringifyJSON (pass tests)

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -11,7 +11,7 @@ describe('stringifyJSON', function() {
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
   });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,64 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+//check for each data type and modify accordingly
+
+  // if obj is null
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (obj === true || obj === false) {
+    return `${obj}`;
+  }
+
+  // if obj is number
+  if (typeof obj === 'number') {
+    return `${obj}`;
+  }
+
+  // if obj is string
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+
+  // if obj is array
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    let result = '';
+    for (let i = 0; i < obj.length; i++) {
+      // if (Array.isArray(obj[i])) {
+      if (i !== obj.length - 1) {
+        result += `${stringifyJSON(obj[i])},`;
+      } else {
+        result += `${stringifyJSON(obj[i])}`;
+      }
+
+    }
+    return `[${result}]`;
+  }
+
+  // if obj is obj
+  if (typeof obj === 'object') {
+    let result = '';
+    if (Object.entries(obj).length === 0) {
+      return '{}';
+    }
+    const entries = Object.entries(obj);
+
+    for (let i = 0; i < entries.length; i++) {
+      if (entries[i][0] !== 'functions' && entries[i][0] !== 'undefined') {
+        if (i < entries.length - 1) {
+          result += `"${entries[i][0]}":${stringifyJSON(entries[i][1])},`;
+        } else {
+          result += `"${entries[i][0]}":${stringifyJSON(entries[i][1])}`;
+        }
+      }
+
+    }
+    return `{${result}}`;
+  }
+
 };
